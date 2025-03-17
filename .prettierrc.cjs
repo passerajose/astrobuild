3592d3e75d499c78492085adad1462a9e58aa78b
passerajose <espinoza.esp123@gmail.com>
2025-03-17 01:13:07 -0300

    Primer commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+	semi: false,
+	singleQuote: true,
+	tabWidth: 2,
+	useTabs: false,
+	plugins: [
+		require('prettier-plugin-astro')
+	],
+	overrides: [
+		{
+			files: '**/*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+}  
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*/*.{astro,html,js,jsx}'],
+  theme: {
+    extend: {
+    }
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/forms')
+  ]
+}
